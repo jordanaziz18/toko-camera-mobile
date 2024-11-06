847b7bca357949e97d3a04cc38d3d0936ab5f36d
Muhammad Jordan <jordan.aziz18@gmail.com>
2024-11-06 13:18:12 +0700

    tugas7

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:toko_camera_mobile/menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Toko Camera',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.grey,
+        ).copyWith(secondary: Colors.grey[900]),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
